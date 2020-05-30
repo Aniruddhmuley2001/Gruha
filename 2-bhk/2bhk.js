7b23763f0903d96f550074cf0a890ab408592dee
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-05-30 23:38:43 +0530

    update 2bhk

--- 2-bhk/2bhk.js
+++ 2-bhk/2bhk.js
@@ -8,7 +8,9 @@ function myFunction() {
   }
 }
 
-
+function thankingMessage(){
+  alert('Thank you for showing your interest. We will shortly share the contact details of the dealer with you, so that you can contact for further discussions')
+}
 
 // Iterative function to implement Binary Search 
 let iterativeFunction = function (arr, x) { 
@@ -38,11 +40,11 @@ return false;
 // Driver code 
 function search_value() {
 const price = document.querySelector("input").value;
-let arr = ['16,00,000', '18,00,000', '19,00,000', '20,00,000', '22,00,000']; 
+let arr = [2500000, 3300000, 3600000, 3900000, 4200000]; 
 let x = price; 
 // console.log(x);
 if (iterativeFunction(arr, x, 0, arr.length-1)) {
-  if(x === '16,00,000'){
+  if(x == 2500000){
     // document.getElementById("property-container").removeChild(document.getElementById("prop-container"));
     const myNode = document.getElementById("property-container");
     while (myNode.lastElementChild) {
@@ -52,11 +54,11 @@ if (iterativeFunction(arr, x, 0, arr.length-1)) {
     first2bhk.id = "prop-container";
     first2bhk.style.color = "#F5F5F5";
     first2bhk.insertAdjacentHTML("afterbegin", 
-    "<h2 style='font-size: 4rem'>Umiya Estate</h2> <img src='D://Projects/Gruha/img/property1.jpg' alt='property image' style='display: inline-block; position: relative; width: 50%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>664 sq. ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>3rd of 8</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 16,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 12,140(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Dindoli, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>1 year</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Fully furnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Park, Security Personnal</h5>")
+    "<h2 style='font-size: 4rem'>Umiya Estate</h2> <img src='D://Projects/Gruha/img/property1.jpg' alt='property image' style='display: inline-block; position: relative; width: 50%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>1043 sq ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>3rd of 8</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 25,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 19,361(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Dindoli, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>1 year</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Fully furnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Park, Security Personnal</h5> <br> <button class='btn btn-success' onclick='thankingMessage()'>Interested</button>")
     document.getElementById("property-container").appendChild(first2bhk);
   }
 
-  else if(x == '18,00,000'){
+  else if(x == 3900000){
     // document.getElementById("property-container").removeChild(document.getElementById("prop-container"));
     const myNode = document.getElementById("property-container");
     while (myNode.lastElementChild) {
@@ -66,11 +68,11 @@ if (iterativeFunction(arr, x, 0, arr.length-1)) {
     second2bhk.id = "prop-container";
     second2bhk.style.color = "#F5F5F5";
     second2bhk.insertAdjacentHTML("afterbegin", 
-    "<h2 style='font-size: 4rem'>Asopalav Apartments</h2> <img src='D://Projects/Gruha/img/oldprop4.jpeg' alt='property image' style='display: inline-block; position: relative; width: 50%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>800 sq. ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>2nd of 10</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 18,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 14,377(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Tadwadi, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>10+ years</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Semi-Furnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Park, Visitor Parking, Piped Gas</h5>")
+    "<h2 style='font-size: 4rem'>Happy Home Nakshatra Nebula</h2> <img src='D://Projects/Gruha/img/oldprop4.jpeg' alt='property image' style='display: inline-block; position: relative; width: 50%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>1219 sq ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>7th of 13</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 39,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 30,662(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Jahangirabad, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>1 year</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Furnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Piped gas, park, Visitor Parking, swimming pool, Club house</h5> <br> <button class='btn btn-success' onclick='thankingMessage()'>Interested</button>")
     document.getElementById("property-container").appendChild(second2bhk);
   }
 
-  else if(x == '19,00,000'){
+  else if(x == 3600000){
     //document.getElementById("property-container").removeChild(document.getElementById("prop-container"));
     const myNode = document.getElementById("property-container");
     while (myNode.lastElementChild) {
@@ -80,11 +82,11 @@ if (iterativeFunction(arr, x, 0, arr.length-1)) {
     third2bhk.id = "prop-container";
     third2bhk.style.color = "#F5F5F5";
     third2bhk.insertAdjacentHTML("afterbegin", 
-    "<h2 style='font-size: 4rem'>Sugat Residency</h2> <img src='D://Projects/Gruha/img/property2.jpg' alt='property image' style='display: inline-block; position: relative; width: 50%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>370 sq. ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>5th of 5</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 19,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 16,293(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Bhatha, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>Under Construction (Possession in Dec 2024)</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Unfurnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, CCTV Camera, RO System, Power Backup</h5>")
+    "<h2 style='font-size: 4rem'>Happy Elegance</h2> <img src='D://Projects/Gruha/img/property2.jpg' alt='property image' style='display: inline-block; position: relative; width: 50%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>1209 sq ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>9th of 13</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 36,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 32,553(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Vesu, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>Under Construction (Possession in Dec 2024)</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Unfurnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Fire Fighting system, park, Visitor Parking, InterCom</h5> <br> <button class='btn btn-success' onclick='thankingMessage()'>Interested</button>")
     document.getElementById("property-container").appendChild(third2bhk);
   }
 
-  else if(x == '20,00,000'){
+  else if(x == 3300000){
     //document.getElementById("property-container").removeChild(document.getElementById("prop-container"));
     const myNode = document.getElementById("property-container");
     while (myNode.lastElementChild) {
@@ -94,11 +96,11 @@ if (iterativeFunction(arr, x, 0, arr.length-1)) {
     fourth2bhk.id = "prop-container";
     fourth2bhk.style.color = "#F5F5F5";
     fourth2bhk.insertAdjacentHTML("afterbegin", 
-    "<h2 style='font-size: 4rem'>Green Paradise</h2> <img src='D://Projects/Gruha/img/prop4.jpg' alt='property image' style='display: inline-block; position: relative; width: 100%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>671 sq. ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>4th of 12</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 20,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 15,279(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Jahangirabad, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>Under Construction</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Unfurnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Parking, East facing, Vitrificial Flooring, Lift, Power Backup</h5>")
+    "<h2 style='font-size: 4rem'>Palladium Residency</h2> <img src='D://Projects/Gruha/img/prop4.jpg' alt='property image' style='display: inline-block; position: relative; width: 100%; height: auto; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>1207 sq ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>3rd of 14</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 33,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 29,361(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Althan, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>New construction</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Unfurnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Gym, InterCom, Visitor Parking, Childrens’ play area</h5> <br> <button class='btn btn-success' onclick='thankingMessage()'>Interested</button>")
     document.getElementById("property-container").appendChild(fourth2bhk);
   }
 
-  else if(x == '22,00,000'){
+  else if(x == 4200000){
     //document.getElementById("property-container").removeChild(document.getElementById("prop-container"));
     const myNode = document.getElementById("property-container");
     while (myNode.lastElementChild) {
@@ -108,7 +110,7 @@ if (iterativeFunction(arr, x, 0, arr.length-1)) {
     fifth2bhk.id = "prop-container";
     fifth2bhk.style.color = "#F5F5F5";
     fifth2bhk.insertAdjacentHTML("afterbegin", 
-    "<h2 style='font-size: 4rem'>Ramaa Residency</h2> <img src='D://Projects/Gruha/img/property5.jpg' alt='property image' style='display: inline-block; position: relative; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>736 sq. ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>7th of 15</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 22,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 17,341(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Jahangirabad, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>Under Construction (Possession in Jan 2022)</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Unfurnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Park, Swimming Pool, Club House</h5>")
+    "<h2 style='font-size: 4rem'>Rameshwaram Residency</h2> <img src='D://Projects/Gruha/img/property5.jpg' alt='property image' style='display: inline-block; position: relative; float: right; max-width: 100%;'> <br> <h5 style='font-size: 2rem'><strong>Carpet Area: </strong>1043 sq. ft.</h5> <br> <h5 style='font-size: 2rem'><strong>Floor: </strong>6th of 11</h5> <br> <h5 style='font-size: 2rem'><strong>Price: </strong>Rs. 42,00,000/- + GST</h5> <br> <h5 style='font-size: 2rem'><strong>EMI: </strong>Rs. 33,546(approx.)</h5> <br> <h5 style='font-size: 2rem'><strong>Address: </strong>Adajan, Surat</h5> <br> <h5 style='font-size: 2rem'><strong>Property Age: </strong>4 years</h5> <br> <h5 style='font-size: 2rem'><strong>Furnishing: </strong>Unfurnished</h5> <br> <h5 style='font-size: 2rem'><strong>Amenities: </strong>Lift, Park</h5> <br> <button class='btn btn-success' onclick='thankingMessage()'>Interested</button>")
     document.getElementById("property-container").appendChild(fifth2bhk);
   }
 } 
