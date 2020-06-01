851e8fbcbbf10503206a52e78bde7deee305fde1
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2020-06-01 13:33:40 +0530

    issue of nav bar cleared

--- 1-bhk/1bhk.js
+++ 1-bhk/1bhk.js
@@ -1,11 +1,11 @@
 /* Toggle between adding and removing the "responsive" class to myTopnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "myTopnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "myTopnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "myTopnav";
+  }
 }
 
 function thankingMessage(){
